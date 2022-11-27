b67e516572d9bbc6f365778f4e8d215bfc91fa60
pan <panjunzhong@outlook.com>
2022-11-27 21:25:32 +0800

    update: fix include path problem

--- packages/c/cppbase/xmake.lua
+++ packages/c/cppbase/xmake.lua
@@ -2,7 +2,7 @@ package("cppbase")
 
     set_urls("https://github.com/open3rds/cppbase.git")
     set_description("base repo for cpp project")
-    add_versions("v0.0.1", "330c7002760a6c7b50e3c47dfe910081747712e1")
+    add_versions("v0.0.2", "b232139ec00a109c5bf613af62715b234192c5d0")
 
     on_install("linux", function (package)
         import("package.tools.xmake").install(package)
