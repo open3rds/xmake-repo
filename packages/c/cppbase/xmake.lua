f4795863ca2ae851564fd7f0341343ee44fb0bc8
pan <panjunzhong@outlook.com>
2022-11-27 19:58:46 +0800

    update: fix cppbase url

--- packages/c/cppbase/xmake.lua
+++ packages/c/cppbase/xmake.lua
@@ -1,6 +1,6 @@
 package("cppbase")
 
-    set_urls("https://github.com/open3rds/cppbase")
+    set_urls("https://github.com/open3rds/cppbase.git")
     set_description("base repo for cpp project")
     add_versions("v0.0.1", "330c7002760a6c7b50e3c47dfe910081747712e1")
 
