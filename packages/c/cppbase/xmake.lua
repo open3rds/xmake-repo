ccbf61400292313609b8e479a641e204b6dfa71b
pan <panjunzhong@outlook.com>
2022-11-27 19:42:43 +0800

    new: add cppbase

---
+++ packages/c/cppbase/xmake.lua
@@ -0,0 +1,11 @@
+package("cppbase")
+
+    set_urls("https://github.com/open3rds/cppbase")
+    set_description("base repo for cpp project")
+    add_versions("v0.0.1", "330c7002760a6c7b50e3c47dfe910081747712e1")
+
+    on_install("linux", function (package)
+        import("package.tools.xmake").install(package)
+    end)
+
+package_end()
